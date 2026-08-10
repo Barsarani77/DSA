dc729d87bc0fd22d16854d1c978e933f34cc056e
Barsa Rani Nayak <nayakbarsarani692@gmail.com>
2026-08-10 21:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (39.26%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        num = 0
+        current = ""
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+
+            elif ch == '[':
+                stack.append((current, num))
+                current = ""
+                num = 0
+
+            elif ch == ']':
+                prev, k = stack.pop()
+                current = prev + current * k
+
+            else:
+                current += ch
+
+        return current
