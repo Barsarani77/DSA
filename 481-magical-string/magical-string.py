6ab20de61cd89ad8c9474683f3f910f9d0b0c257
Barsa Rani Nayak <nayakbarsarani692@gmail.com>
2026-08-27 23:47:04 +0530

    Time: 43 ms (92.38%) | Memory: 20.6 MB (18.81%) - LeetSync

---
+++ 481-magical-string/magical-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def magicalString(self, n: int) -> int:
+        if n <= 0:
+            return 0
+
+        s = [1, 2, 2]
+
+        i = 2
+        num = 1
+
+        while len(s) < n:
+            count = s[i]
+
+            for _ in range(count):
+                s.append(num)
+
+            num = 3 - num
+            i += 1
+
+        return s[:n].count(1)
